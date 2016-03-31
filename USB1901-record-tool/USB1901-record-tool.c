b5b5763fbb9511ab5f419d7a3638d0708b29f4be
Anders Gidenstam <anders.gidenstam@hb.se>
2016-03-31 10:35:02 +0200

    Added the selected license: GPL v3.

--- USB1901-record-tool/USB1901-record-tool.c
+++ USB1901-record-tool/USB1901-record-tool.c
@@ -1,5 +1,25 @@
+/*
+ * Windows command line utility to record analog input samples in differential
+ * mode from a USB-1901 DAQ in double buffered mode.
+ *
+ * Copyright (C) 2014, 2016  Anders Gidenstam, Chalmers University of Technology
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 /*----------------------------------------------------------------------------*/
-/* Anders Gidenstam for the EXCESS project, 2014                              */
+/* Anders Gidenstam for the EXCESS project  2014, 2016                        */
 /*                                                                            */
 /* This program records analog input samples in differential mode from a      */
 /* USB-1901 in double buffered mode.                                          */
